9fcfb51aa306dd6b43673688f038d2e8f279ea1f
17861691611 <sundada1611@163.com>
2022-09-23 00:25:47 +0800

    111

---
+++ GAN_MNIST.py
@@ -0,0 +1,201 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+import pandas, numpy, random
+import matplotlib.pyplot as plt
+
+
+class MnistDataset(Dataset):
+
+    def __init__(self, csv_file):
+        self.data_df = pandas.read_csv(csv_file, header=None)
+        pass
+
+    def __len__(self):
+        return len(self.data_df)
+
+    def __getitem__(self, index):
+        # image target (label)
+        label = self.data_df.iloc[index, 0]
+        target = torch.zeros((10))
+        target[label] = 1.0
+
+        # image data, normalised from 0-255 to 0-1
+        image_values = torch.FloatTensor(self.data_df.iloc[index, 1:].values) / 255.0
+
+        # return label, image data tensor and target tensor
+        return label, image_values, target
+
+    def plot_image(self, index):
+        img = self.data_df.iloc[index, 1:].values.reshape(28, 28)
+        plt.title("label = " + str(self.data_df.iloc[index, 0]))
+        plt.imshow(img, interpolation='none', cmap='Blues')
+        pass
+
+    pass
+
+# 加载数据
+mnist_dataset = MnistDataset('./data/mnist/mnist_train.csv')
+# 输出数据查看
+mnist_dataset.plot_image(17)
+
+# functions to generate random data
+# 生成随机数据
+# def generate_random_image(size):
+#     random_data = torch.rand(size)
+#     return random_data
+
+def generate_random_seed(size):
+    random_data = torch.rand(size)
+    return random_data
+
+# 判别器
+class Discriminator(nn.Module):
+
+    def __init__(self):
+        # initialise parent pytorch class
+        super().__init__()
+
+        # define neural network layers
+        self.model = nn.Sequential(
+            nn.Linear(784, 200),
+            nn.LeakyReLU(0.02),
+            nn.LayerNorm(200),
+            nn.Linear(200, 1),
+            nn.Sigmoid()
+        )
+
+        # create loss function
+        self.loss_function = nn.BCELoss()
+
+        # create optimiser, simple stochastic gradient descent
+        self.optimiser = torch.optim.Adam(self.parameters(), lr=0.0001)
+
+        # counter and accumulator for progress
+        # 进度计数器和累加器
+        self.counter = 0;
+        self.progress = []
+
+        pass
+
+    def forward(self, inputs):
+        # simply run model
+        return self.model(inputs)
+
+    def train(self, inputs, targets):
+        # calculate the output of the network
+        outputs = self.forward(inputs)
+        # 计算损失函数
+        loss = self.loss_function(outputs, targets)
+        # 每累计10次错误增加计数器，每一万次输出训练进度
+        self.counter += 1
+        if (self.counter % 10 == 0):
+            self.progress.append(loss.item())
+            pass
+        if (self.counter % 10000 == 0):
+            print("counter = ", self.counter)
+            pass
+        # zpero gradients, erform a backward pass, update weights
+        self.optimiser.zero_grad()
+        loss.backward()
+        self.optimiser.step()
+
+        pass
+
+    def plot_progress(self):
+        df = pandas.DataFrame(self.progress, columns=['loss'])
+        df.plot(ylim=(0), figsize=(16, 8), alpha=0.1, marker='.', grid=True, yticks=(0, 0.25, 0.5, 1.0, 5.0))
+        pass
+
+    pass
+
+# 生成器
+class Generator(nn.Module):
+
+    def __init__(self):
+        # initialise parent pytorch class
+        super().__init__()
+
+        # define neural network layers
+        self.model = nn.Sequential(
+            nn.Linear(1, 200),
+            nn.LeakyReLU(0.02),
+            nn.LayerNorm(200),
+            nn.Linear(200, 784),
+            nn.Sigmoid()
+        )
+
+        # create optimiser, simple stochastic gradient descent
+        self.optimiser = torch.optim.Adam(self.parameters(), lr=0.0001)
+
+        # counter and accumulator for progress
+        self.counter = 0;
+        self.progress = []
+
+        pass
+
+    def forward(self, inputs):
+        # simply run model
+        return self.model(inputs)
+
+    def train(self, D, inputs, targets):
+        # calculate the output of the network
+        g_output = self.forward(inputs)
+        # pass onto Discriminator
+        d_output = D.forward(g_output)
+        # calculate error
+        loss = D.loss_function(d_output, targets)
+
+        # increase counter and accumulate error every 10
+        self.counter += 1;
+        if (self.counter % 10 == 0):
+            self.progress.append(loss.item())
+            pass
+        # zero gradients, perform a backward pass, update weights
+        self.optimiser.zero_grad()
+        loss.backward()
+        self.optimiser.step()
+
+        pass
+
+    def plot_progress(self):
+        df = pandas.DataFrame(self.progress, columns=['loss'])
+        df.plot(ylim=(0), figsize=(16, 8), alpha=0.1, marker='.', grid=True, yticks=(0, 0.25, 0.5, 1.0, 5.0))
+        pass
+
+    pass
+
+
+
+# 训练判别器，生成器
+D = Discriminator()
+G = Generator()
+epochs = 2
+for epoch in range(epochs):
+    print("epoch = ", epoch + 1)
+    # train Discriminator and Generator
+    for label, image_data_tensor, target_tensor in mnist_dataset:
+        # 用真实图片训练判别器
+        D.train(image_data_tensor, torch.FloatTensor([1.0]))
+        # 用生成器的虚假图片训练判别器
+        D.train(G.forward(generate_random_seed(1)).detach(), torch.FloatTensor([0.0]))
+        # 训练生成器
+        G.train(D, generate_random_seed(1), torch.FloatTensor([1.0]))
+        pass
+    pass
+
+# 绘制损失函数
+D.plot_progress()
+G.plot_progress()
+
+# 运行生成器，绘制出来图片
+f, axarr = plt.subplots(2,3, figsize=(16,8))
+for i in range(2):
+    for j in range(3):
+        output = G.forward(generate_random_seed(1))
+        img = output.detach().numpy().reshape(28,28)
+        axarr[i,j].imshow(img, interpolation='none', cmap='Blues')
+        pass
+    pass
+plt.show()
